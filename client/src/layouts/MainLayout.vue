dd98dfe46ef9a5427c5e76cc9f6c254bdc3b2101
Maia Everett <maia@everett.one>
2021-08-23 20:27:11 +0700

    Responsive app toolbar

--- client/src/layouts/MainLayout.vue
+++ client/src/layouts/MainLayout.vue
@@ -11,10 +11,32 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
+        <div class="layout__filler"></div>
+
+        <q-toolbar-title class="layout__toolbar-title text-center">
           Chaos Archives
         </q-toolbar-title>
 
+        <div class="layout__filler text-right">
+          <q-btn-group flat class="gt-sm">
+            <q-btn v-for="link in navbarLinks" stretch flat :key="link.label" :label="link.label" :to="link.to" />
+          </q-btn-group>
+          <q-btn-dropdown flat class="lt-md" icon="more_horiz">
+            <q-list>
+              <q-item
+                v-for="link in navbarLinks"
+                clickable
+                v-close-popup
+                :key="link.label"
+                :to="link.to"
+              >
+                <q-item-section>
+                  <q-item-label>{{link.label}}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </div>
         <q-btn dense flat round icon="event" @click="toggleRightDrawer" />
       </q-toolbar>
     </q-header>
@@ -69,6 +91,12 @@ import { Vue, Options } from 'vue-class-component'
 export default class MainLayout extends Vue {
   leftDrawerOpen = false;
   rightDrawerOpen = false;
+  navbarLinks = [
+    { label: 'About', to: '' },
+    { label: 'Terms', to: '' },
+    { label: 'FAQ', to: '' },
+    { label: 'Contact', to: '' },
+  ];
 
   toggleLeftDrawer () {
     this.leftDrawerOpen = !this.leftDrawerOpen
@@ -79,3 +107,15 @@ export default class MainLayout extends Vue {
   }
 }
 </script>
+
+<style>
+  .layout__filler {
+    flex-basis: 0;
+    flex-grow: 1;
+  }
+
+  .layout__toolbar-title {
+    flex-basis: inherit;
+    flex-grow: 0;
+  }
+</style>
