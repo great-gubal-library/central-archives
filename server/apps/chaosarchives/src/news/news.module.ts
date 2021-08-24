361d89692d70b7b4efb2f4f37e8f73665f478fac
Maia Everett <maia@everett.one>
2021-08-24 22:45:53 +0700

    News fetcher API

--- server/apps/chaosarchives/src/news/news.module.ts
+++ server/apps/chaosarchives/src/news/news.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 
 @Module({
   imports: [
-    
+    HttpModule,   
   ],
   controllers: [NewsController],
   providers: [NewsService],
--- server/apps/chaosarchives/src/news/news.service.ts
+++ server/apps/chaosarchives/src/news/news.service.ts
@@ -1,16 +1,40 @@
 import { InjectRedis, Redis } from '@nestjs-modules/ioredis';
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import parse from 'node-html-parser';
 import { NewsEntry } from './dto/NewsEntry';
 
 @Injectable()
 export class NewsService {
+	private readonly NEWS_SITE = 'https://crescentmoonpublishing.com/the-daily-moogle/';
+
 	constructor(
     @InjectRedis()
     private readonly redisService: Redis,
+		private httpService: HttpService,
 	) {}
 
 	async getNews(): Promise<NewsEntry[]> {
-		await this.redisService.set("mykey", 10);
-		return [];
+		const response = await this.httpService.get<string>(this.NEWS_SITE).toPromise();
+		const doc = parse(response.data);
+		const newsElement = doc.querySelector('.grid-col-desk-2');
+		const newsItems = newsElement.querySelectorAll('.jet-listing-grid__item');
+
+		const imageRegex = /background-image:url\("(http[^"]*)"\)/;
+		
+		return newsItems.slice(0, 3).map(item => {
+			const style = item.querySelector("style").textContent;
+			const imageMatch = style.match(imageRegex);
+			const titleLink = item.querySelector('.elementor-heading-title a');
+			const authorLink = item.querySelector('.jet-listing-dynamic-field__content a');
+			const content = item.querySelector('.elementor-text-editor');
+
+			return {
+				title: titleLink.textContent || '',
+				author: authorLink ? authorLink.textContent : '',
+				content: content ? content.textContent.trim() : '',
+				image: imageMatch ? imageMatch[1] : '',
+				link: titleLink.getAttribute('href') || '',
+			};
+		});
 	}
 }
