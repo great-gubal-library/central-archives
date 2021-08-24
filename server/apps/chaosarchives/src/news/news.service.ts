054803d7aa243fd242f94848eb5824ee8c26bd41
Maia Everett <maia@everett.one>
2021-08-24 23:03:39 +0700

    Cache news in Redis

--- server/apps/chaosarchives/src/news/news.service.ts
+++ server/apps/chaosarchives/src/news/news.service.ts
@@ -8,19 +8,32 @@ export class NewsService {
 	private readonly NEWS_SITE = 'https://crescentmoonpublishing.com/the-daily-moogle/';
 
 	constructor(
-    @InjectRedis()
-    private readonly redisService: Redis,
+		@InjectRedis()
+		private readonly redisService: Redis,
 		private httpService: HttpService,
-	) {}
+	) { }
 
 	async getNews(): Promise<NewsEntry[]> {
+		const cachedNews = await this.redisService.get('news');
+
+		if (cachedNews) {
+			return JSON.parse(cachedNews);
+		}
+
+		// Not cached - fetch and cache
+		const news = await this.fetchNews();
+		this.redisService.set('news', JSON.stringify(news), 'ex', 5 * 60); // Intentionally no await
+		return news;
+	}
+
+	private async fetchNews(): Promise<NewsEntry[]> {
 		const response = await this.httpService.get<string>(this.NEWS_SITE).toPromise();
 		const doc = parse(response.data);
 		const newsElement = doc.querySelector('.grid-col-desk-2');
 		const newsItems = newsElement.querySelectorAll('.jet-listing-grid__item');
 
 		const imageRegex = /background-image:url\("(http[^"]*)"\)/;
-		
+
 		return newsItems.slice(0, 3).map(item => {
 			const style = item.querySelector("style").textContent;
 			const imageMatch = style.match(imageRegex);
