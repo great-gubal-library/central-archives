a7982edfb69bc61fa468566349bcc9e54bcc2d37
Maia Everett <maia@everett.one>
2021-08-24 18:38:54 +0700

    Refactor news timeline into a separate component

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,24 +1,6 @@
 <template>
   <q-page>
-    <q-timeline>
-      <q-timeline-entry heading>
-        News by The Daily Moogle
-      </q-timeline-entry>
-      <q-timeline-entry v-for="(entry, index) in news" :key="index" :subtitle="entry.author">
-        <template v-slot:title>
-           <a :href="entry.link" target="_blank">{{entry.title}}</a>
-        </template>
-        <div class="row justify-center">
-          <div class="col" style="max-width: 300px; min-width: 200px">
-            <q-img :src="entry.image" />
-          </div>
-          <div class="col" style="padding-left: 6px; min-width: 300px">
-            {{entry.content}}
-            <p><a :href="entry.link" target="_blank">Read more...</a></p>
-          </div>
-        </div>
-      </q-timeline-entry>
-    </q-timeline>
+    <news-timeline />
     <section>
       <h5 style="margin: 1rem">Latest adventures</h5>
       <q-markup-table dense>
@@ -184,35 +166,14 @@
 
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component'
+import NewsTimeline from 'components/mainpage/NewsTimeline.vue'
 
 @Options({
-  components: {  }
+  components: {
+    NewsTimeline
+  }
 })
 export default class PageIndex extends Vue {
-  readonly news = [
-    {
-      title: 'Travelers Be Warned, Coerthas Unsafe!',
-      author: 'Kiyo Obinata',
-      content: 'Lately the Coerthan lands held in the grip of perpetual winter have quite possibly become home to something more ominous than the biting cold. Indeed there are rumors that some lands in the Western Highlands might even be cursed!',
-      image: 'https://crescentmoonpublishing.com/wp-content/uploads/2021/08/2021-08-18_21-30-33-156_Fairy-Gameplay.png',
-      link: 'https://crescentmoonpublishing.com/travelers-be-warned-coerthas-unsafe/'
-    },
-    {
-      title: 'Letters to the Editor',
-      author: 'Jonahson Jameson',
-      content: 'Lately the Coerthan lands held in the grip of perpetual winter have quite possibly become home to something more ominous than the biting cold. Indeed there are rumors that some lands in the Western Highlands might even be cursed!',
-      image: 'https://crescentmoonpublishing.com/wp-content/uploads/jet-engine-forms/24/2021/08/2021-04-29_20-12-29-921_Fairy_Gameplay.png',
-      link: 'https://crescentmoonpublishing.com/letters-to-the-editor-2/'
-    },
-    {
-      title: 'All things… Wes?',
-      author: 'Wes, the Butterfly Man',
-      content: 'The Butterfly Man answers two requests from our readers, including some questions about himself.',
-      image: 'https://crescentmoonpublishing.com/wp-content/uploads/jet-engine-forms/18/2021/08/ffxiv_26062021_222206_967.png',
-      link: 'https://crescentmoonpublishing.com/letters-to-the-editor-2/'
-    }
-  ];
-
   readonly adventures = [
     { title: 'Lurkers in the Grotto', author: 'Vielle Janlenoux', date: '1 hour ago' },
     { title: 'Dressed to Call', author: 'Vielle Janlenoux', date: '2 hours ago' },
