01e70645af54f2461cbe090f028720af62887dff
Maia Everett <maia@everett.one>
2021-08-30 22:25:14 +0700

    Add xivapi-js dependency

---
+++ server/types/xivapi.d.ts
@@ -0,0 +1,49 @@
+declare module '@xivapi/js' {
+  interface PagedResult<T> {
+    Pagination: {
+      Page: number;
+      PageNext: number | null;
+      PagePrev: number | null;
+      PageTotal: number;
+      Results: number;
+      ResultsPerPage: number;
+      ResultsTotal: number;
+    };
+    Results: T[];
+  }
+
+  interface CharacterSearchEntry {
+    Avatar: string;
+    FeastMatches: number;
+    ID: number;
+    Lang: string;
+    Name: string;
+    Rank: number | null;
+    RankIcon: string | null;
+    Server: string;
+  }
+
+  interface CharacterData {
+    Character: {
+      Avatar: string;
+      DC: string;
+      ID: number;
+      Name: string;
+      Race: number;
+      Server: string;
+    };
+  }
+
+  export default class XIVAPI {
+    character: {
+      get(
+        lodestoneId: number,
+        options: { extended: 0 | 1 },
+      ): Promise<CharacterData>;
+      search(
+        name: string,
+        options?: { server: string },
+      ): Promise<PagedResult<CharacterSearchEntry>>;
+    };
+  }
+}
