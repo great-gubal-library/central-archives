840b2355234de83bea21a4a1d667c2ca893193ec
Maia Everett <maia@everett.one>
2021-09-28 18:01:16 +0700

    Update links on about page

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -2,10 +2,10 @@
   <q-page>
 		<h2>About Chaos Archives</h2>
 		<p>
-			Chaos Archives is an initiative to create a roleplaying portal for the Chaos (EU) roleplaying community in Final Fantasy XIV, heavily inspired by <a href="https://www.argentarchives.org">Argent Archives</a>, but built from scratch to cater to the needs of FFXIV roleplayers.
+			Chaos Archives is an initiative to create a roleplaying portal for the Chaos (EU) roleplaying community in Final Fantasy XIV, heavily inspired by <a href="https://www.argentarchives.org">Argent Archives <q-icon name="link" /></a>, but built from scratch to cater to the needs of FFXIV roleplayers.
 		</p>
 		<p>
-			It is currently in development. Check the <code>#gubal-dev</code> channel on the Chaos Discord server to keep up with the project's progress.
+			It is currently in development. Check the <code>#gubal-dev</code> channel on the <a href="https://discord.gg/dWDKxhV" target="_blank">Chaos Roleplaying <q-icon name="discord" /></a> Discord server to keep up with the project's progress.
 		</p>
 	</q-page>	
 </template>
