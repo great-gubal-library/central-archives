0f2f254f4b61df6a0a500d49a96a2c5f5aa805ef
Maia Everett <maia@everett.one>
2021-10-10 23:13:09 +0700

    Update Discord link

--- client/src/pages/Contact.vue
+++ client/src/pages/Contact.vue
@@ -5,7 +5,7 @@
 			The primary developer and administrator of Chaos Archives is <router-link to="/Omega/Vielle_Janlenoux">Vielle Janlenoux</router-link>, also known as Lintian Wintermist. I can be contacted on Discord as <strong>Lintian#3278</strong> or by <a href="mailto:admin@chaosarchives.org">email <q-icon name="mail_outline" /></a>.
 		</p>
 		<p>
-			Chaos Archives are part of the <a href="https://discord.gg/dWDKxhV" target="_blank">Chaos Roleplaying <q-icon name="discord" /></a> Discord community. Visit the <code>#gubal-dev</code> channel to discuss its development.
+			Chaos Archives are part of the <a href="https://discord.gg/ffxiv-chaos-rp" target="_blank">Chaos Roleplaying <q-icon name="discord" /></a> Discord community. Visit the <code>#gubal-dev</code> channel to discuss its development.
 		</p>
 		<h5>Bug reporting</h5>
 		<p>
