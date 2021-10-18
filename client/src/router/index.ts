aaefc079055e9cb98d651d3ee7663110745397c2
Maia Everett <maia@everett.one>
2021-10-18 22:45:53 +0700

    Remember scroll position on navigation

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -24,7 +24,7 @@ export default route<StateInterface>(function (/* { store, ssrContext } */) {
     : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory);
 
   const router = createRouter({
-    scrollBehavior: () => ({ left: 0, top: 0 }),
+    scrollBehavior: (_, __, savedPosition) => savedPosition || { left: 0, top: 0 },
     routes,
 
     // Leave this as is and make changes in quasar.conf.js instead!
