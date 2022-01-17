49f5637596689194430f5c04d80b61922521b927
Maia Everett <maia@everett.one>
2022-01-17 16:21:43 +0700

    Fix Discord community link

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -5,7 +5,7 @@
 			Chaos Archives is a roleplay portal for the Chaos (EU) roleplaying community in Final Fantasy XIV, heavily inspired by <a href="https://www.argentarchives.org">Argent Archives <q-icon class="external-link-icon" name="launch" /></a>, but built from scratch to cater to the needs of FFXIV roleplayers.
 		</p>
 		<p>
-			Chaos Archives is part of the <a href="https://discord.gg/ffxiv-chaos-rp" target="_blank">FFXIV Chaos RP Community <q-icon name="discord" /></a>. Feel free to join us on Discord!
+			Chaos Archives is part of the <a href="https://discord.gg/rCHdUdSVsr" target="_blank">FFXIV Chaos RP Community <q-icon name="discord" /></a>. Feel free to join us on Discord!
 		</p>
 		<h3>Credits</h3>
 		<dl>
@@ -21,7 +21,7 @@
 		</p>
 		<h3>Useful Links</h3>
 		<dl>
-			<dt><a href="https://discord.gg/ffxiv-chaos-rp" target="_blank">FFXIV Chaos Roleplaying Community <q-icon name="discord" /></a></dt>
+			<dt><a href="https://discord.gg/rCHdUdSVsr" target="_blank">FFXIV Chaos Roleplaying Community <q-icon name="discord" /></a></dt>
 			<dd>Our Discord server.</dd>
 			<dt><a href="https://ffxiv-roleplayers.com/topic/1674-the-role-play-handbook/" target="_blank">The Role-Play Handbook <q-icon class="external-link-icon" name="launch" /></a></dt>
 			<dd>Introduction to RP in MMO games, and in FFXIV in particular.</dd>
