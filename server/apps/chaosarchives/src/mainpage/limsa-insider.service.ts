9f4f2c98d5ee64a4dc145e820f109d149d5220ae
Maia Everett <maia@everett.one>
2022-01-25 14:41:38 +0700

    Fix Limsa Insider parser to avoid latest issue on newsarchive

--- server/apps/chaosarchives/src/mainpage/limsa-insider.service.ts
+++ server/apps/chaosarchives/src/mainpage/limsa-insider.service.ts
@@ -15,7 +15,7 @@ for (const ordinal of [ 'first', 'second', 'third', 'fourth', 'fifth', 'sixth' ]
 export class LimsaInsiderService {
 	private readonly log = new Logger(LimsaInsiderService.name);
 
-	private readonly NEWS_SITE = 'https://newsarchive.kryst.company/';
+	private readonly ARCHIVE_NEWS_SITE = 'https://newsarchive.kryst.company/';
 	
 	private readonly CURRENT_NEWS_SITE = 'https://news.kryst.company/';
 	
@@ -24,40 +24,64 @@ export class LimsaInsiderService {
 	) { }
 
 	async fetchNews(): Promise<NewsDto[]> {
-		// Parse Carrd page
-		const page = await this.httpService.get<string>(this.NEWS_SITE).toPromise();
-		const doc = parse(page.data);
-		const latestIssues = doc.querySelectorAll('.inner section').reverse();
+		const result: NewsDto[] = [];
 
-		return latestIssues.slice(0, SharedConstants.MAX_NEWS_ENTRIES).map((issue, index) => {
-			const title = issue.querySelector('h2').textContent;
-			const content = issue.querySelector('h2 + p').textContent;
-			const author = issue.querySelector('h2 + p + div.container').querySelector('u').textContent.replace(/^By /, '');
-			const anchor = issue.id.replace(/-section$/, '');
-			const link = index === 0 ? this.CURRENT_NEWS_SITE : `${this.NEWS_SITE}#${anchor}}`;
-			const image = this.getImageUrl(issue.querySelector('img'));
-			const [ month, day ] = issue.querySelectorAll('.container p');
-			const date = (this.eorzeanToGregorianDate(month.textContent, day.textContent) || DateTime.now()).toMillis();
+		// Parse Carrd pages
+		const [ currentPage, archivePage ] = await Promise.all([
+			this.httpService.get<string>(this.CURRENT_NEWS_SITE).toPromise(),
+			this.httpService.get<string>(this.ARCHIVE_NEWS_SITE).toPromise(),
+		]);
 
-			return {
-				title,
-				content,
-				author,
-				link,
-				image,
-				date,
-			};
-		});
+		const currentDoc = parse(currentPage.data);
+		const currentIssue = currentDoc.querySelector('.inner');
+		result.push(this.parseIssue(currentIssue, 0));
+
+		const archiveDoc = parse(archivePage.data);
+		const archiveIssues = archiveDoc.querySelectorAll('.inner section').reverse();
+		// ignore latest hidden entry, as it can be wrong compared to the version on news.kryst.company
+		result.push(...archiveIssues.slice(1, SharedConstants.MAX_NEWS_ENTRIES)
+				.map((issue, index) => this.parseIssue(issue, index + 1)));
+
+		return result;
 	}
 
-	private getImageUrl(image: HTMLElement): string {
+	private parseIssue(issue: HTMLElement, index: number): NewsDto {
+		let selector = 'hr + h2';
+		let titleElement = issue.querySelector(selector);
+
+		if (!titleElement) {
+			selector = 'h2';
+			titleElement = issue.querySelector(selector);
+		}
+
+		const title = titleElement.textContent;
+		const content = issue.querySelector(`${selector} + p`).textContent;
+		const author = issue.querySelector(`${selector} + p + div.container`).querySelector('u').textContent.replace(/^By /, '');
+		const anchor = issue.id.replace(/-section$/, '');
+		const link = index === 0 ? this.CURRENT_NEWS_SITE : `${this.ARCHIVE_NEWS_SITE}#${anchor}}`;
+		const image = this.getImageUrl(issue.querySelector('img'),
+				index === 0 ? this.CURRENT_NEWS_SITE : this.ARCHIVE_NEWS_SITE);
+		const [ month, day ] = issue.querySelectorAll('.container p');
+		const date = (this.eorzeanToGregorianDate(month.textContent, day.textContent) || DateTime.now()).toMillis();
+
+		return {
+			title,
+			content,
+			author,
+			link,
+			image,
+			date,
+		};
+	}
+
+	private getImageUrl(image: HTMLElement, baseUrl: string): string {
 		const imageUrl = image.getAttribute('data-src') || image.getAttribute('src')!;
 
 		if (imageUrl.startsWith('https://')) {
 			return imageUrl;
 		}
 
-		return `${this.NEWS_SITE}${imageUrl}`;
+		return `${baseUrl}${imageUrl}`;
 	}
 
 	private eorzeanToGregorianDate(month: string, day: string): DateTime|null {
