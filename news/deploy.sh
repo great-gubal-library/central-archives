3eaeba786ef8eb51f0fdee461030a40df531c206
Maia Everett <maia@everett.one>
2022-05-28 18:48:04 +0700

    Correct deployment script for The Harborwatch

--- news/deploy.sh
+++ news/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SSH_SERVER=`cat server.txt`
-REMOTE_DIR=/var/www/chaosarchives
+REMOTE_DIR=/var/www/harborwatch
 
 echo "Copying files to server"
 rsync -rlptv -e ssh --delete dist/spa/ $SSH_SERVER:$REMOTE_DIR
