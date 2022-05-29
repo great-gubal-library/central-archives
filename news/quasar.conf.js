ad5fe495a95f48cdbef38f582a5e5d9f66c893e8
Maia Everett <maia@everett.one>
2022-05-30 01:20:41 +0700

    Move API to separate common package, enable API use in news

--- news/quasar.conf.js
+++ news/quasar.conf.js
@@ -30,7 +30,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli/boot-files
     boot: [
-      //'axios',
+      'axios',
       //'display',
       //'polyfills',
     ],
