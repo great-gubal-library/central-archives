ef862519c2835ae03f188d741a12151e0e5796e4
Maia Everett <maia@everett.one>
2022-06-13 23:18:56 +0700

    Tweak My Articles page

--- news/src/pages/MyArticles.vue
+++ news/src/pages/MyArticles.vue
@@ -1,22 +1,24 @@
 <template>
   <section class="page-my-articles">
-		<h2 class="with-subtitle">My Articles</h2>
-		<div class="subtitle">for {{ $store.getters.character?.newsPseudonym || $store.getters.character?.name }}</div>
-		<p class="text-right">
-			<q-btn color="secondary" label="Submit new article" icon="add" to="/create-article" />
-		</p>
-		<q-list bordered class="striped-list">
-			<q-item v-for="article in articles" :key="article.id">
-				<q-item-section>
-					<q-item-label>{{article.title}}</q-item-label>
-					<q-item-label caption>{{$display.articleStatuses[article.status]}}</q-item-label>
-        </q-item-section>
-        <q-item-section side>
-					<q-btn v-if="article.canEdit" flat color="primary" label="Edit" :to="`/edit-article/${article.id}`" />
-        </q-item-section>
-			</q-item>
-		</q-list>
-	</section>
+    <h2 class="with-subtitle">My Articles</h2>
+    <div class="subtitle">for {{ $store.getters.character?.newsPseudonym || $store.getters.character?.name }}</div>
+    <section class="page-my-articles__content">
+      <p class="text-right">
+        <q-btn color="secondary" label="Submit new article" icon="add" to="/create-article" />
+      </p>
+      <q-list bordered class="striped-list">
+        <q-item v-for="article in articles" :key="article.id">
+          <q-item-section>
+            <q-item-label>{{ article.title }}</q-item-label>
+            <q-item-label caption>{{ $display.articleStatuses[article.status] }}</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-btn v-if="article.canEdit" flat color="primary" label="Edit" :to="`/edit-article/${article.id}`" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </section>
+  </section>
 </template>
 
 <script lang="ts">
@@ -32,34 +34,34 @@ const $router = useRouter();
 const $store = useStore();
 
 async function load(): Promise<NewsArticleDto[]> {
-	try {
-		return await $api.news.getMyArticles($store.getters.characterId!);
-	} catch (e) {
-		notifyError(e);
-		void $router.replace('/');
-		throw e;
-	}
+  try {
+    return await $api.news.getMyArticles($store.getters.characterId!);
+  } catch (e) {
+    notifyError(e);
+    void $router.replace('/');
+    throw e;
+  }
 }
 
 @Options({
-	name: 'PageMyArticles',
-	async beforeRouteEnter(_, __, next) {
-		const articles = await load();
-		next(vm => vm.setContent(articles));
-	}
+  name: 'PageMyArticles',
+  async beforeRouteEnter(_, __, next) {
+    const articles = await load();
+    next((vm) => vm.setContent(articles));
+  },
 })
 export default class PageMyArticles extends Vue {
-	articles: NewsArticleDto[] = [];
+  articles: NewsArticleDto[] = [];
 
-	setContent(articles: NewsArticleDto[]) {
-		this.articles = articles;
-	}
+  setContent(articles: NewsArticleDto[]) {
+    this.articles = articles;
+  }
 }
 </script>
 
 <style lang="scss">
-.page-my-articles .q-list {
-	max-width: 700px;
-	margin: auto;
+.page-my-articles__content {
+  max-width: 700px;
+  margin: auto;
 }
 </style>
