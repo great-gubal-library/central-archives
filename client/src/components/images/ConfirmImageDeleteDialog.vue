58e1f2d57183c3506ad62f90d180e20f560a872d
Queen <62080643+HellaRoyal@users.noreply.github.com>
2022-12-11 23:22:53 +0100

    Update ConfirmImageDeleteDialog.vue

--- client/src/components/images/ConfirmImageDeleteDialog.vue
+++ client/src/components/images/ConfirmImageDeleteDialog.vue
@@ -3,13 +3,13 @@
     <q-card class="confirm-image-delete-dialog">
 			<h5>Confirm Image Deletion</h5>
 			<q-img :src="image.url" fit="contain" />
-			<p>Are you sure you want to delete this image? <strong>This operation cannot be undone.</strong></p>
+			<p>Möchtest du dieses Bild wirklich löschen? <strong>Dieser Vorgang kann nicht rückgängig gemacht werden.</strong></p>
       <p v-if="forceConfirming">
-        <strong>{{forceConfirmMessage}}.</strong> Do you still want to delete it and unlink it from every page that uses it as a banner?
-      </p>
+        <strong>{{forceConfirmMessage}}.</strong> Möchtest du dieses Bild trotzdem löschen und von jeder Seite entfernen?
+        </p>
 			<q-card-actions align="right">
 				<q-btn flat color="primary" label="Cancel" @click="onCancelClick" />
-				<q-btn flat color="negative" :label="!forceConfirming ? 'Delete' : 'Delete Anyway'" @click="onDeleteClick" />
+				<q-btn flat color="negative" :label="!forceConfirming ? 'Löschen' : 'Trotzdem löschen'" @click="onDeleteClick" />
 			</q-card-actions>
 			<q-inner-loading :showing="deleting" />
     </q-card>
