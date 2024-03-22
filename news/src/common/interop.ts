7f6a970a7ea4036a252b16628cb96ebc8a3da865
Maia Everett <maia@everett.one>
2024-03-22 23:23:14 +0200

    Deduplicate hardcoded domains

--- news/src/common/interop.ts
+++ news/src/common/interop.ts
@@ -1,5 +1,10 @@
+import SharedConstants from '@app/shared/SharedConstants';
+import { SiteRegion } from '@app/shared/enums/region.enum';
+
 // TODO: Hardcoded for now
 const isLocal = window.location.protocol === 'http:' && window.location.port === '8081';
+const euDomain = SharedConstants.regions[SiteRegion.EU].domain;
+const globalDomain = SharedConstants.regions[SiteRegion.GLOBAL].domain;
 
-export const CHAOS_ARCHIVES_ROOT = isLocal ? 'http://local.chaosarchives.org:8080' : 'https://chaosarchives.org';
-export const CENTRAL_ARCHIVES_ROOT = isLocal ? 'http://local.centralarchives.org:8080' : 'https://centralarchives.org';
+export const CHAOS_ARCHIVES_ROOT = isLocal ? `http://${euDomain}:8080` : `https://${euDomain}`;
+export const CENTRAL_ARCHIVES_ROOT = isLocal ? `http://${globalDomain}:8080` : `https://${globalDomain}`;
