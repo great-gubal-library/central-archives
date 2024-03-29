0faf6108f75bf771a961686c389756239ab9e6c5
Maia Everett <maia@everett.one>
2024-03-29 02:28:46 +0200

    Remove now-unused dependency

--- server/libs/authorization/src/authorization.module.ts
+++ server/libs/authorization/src/authorization.module.ts
@@ -1,30 +1,20 @@
-import { authConfiguration } from '@app/configuration';
 import { Character, User } from '@app/entity';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthorizationImplService as AuthorizationImplService } from './impl/authorization-impl.service';
-import { JwtAuthGuard } from './guards/jwt-auth.guard';
-import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthorizationService } from './authorization.service';
-import { RolesGuard } from './guards/role.guard';
-import { JwtFormStrategy } from './strategies/jwt-form.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtFormAuthGuard } from './guards/jwt-form-auth.guard';
-import { JwtStrategyImpl } from './strategies/jwt-strategy.impl';
+import { RolesGuard } from './guards/role.guard';
+import { AuthorizationImplService } from './impl/authorization-impl.service';
 import { JweService } from './impl/jwe.service';
+import { JwtFormStrategy } from './strategies/jwt-form.strategy';
+import { JwtStrategyImpl } from './strategies/jwt-strategy.impl';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, Character]),
-    JwtModule.registerAsync({
-      useFactory: () => ({
-        secret: authConfiguration.jwtSecret,
-        signOptions: {
-          expiresIn: authConfiguration.jwtExpiry,
-        },
-      }),
-    }),
   ],
   providers: [
     AuthorizationImplService,
